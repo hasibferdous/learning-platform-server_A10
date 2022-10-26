0220172f7dc145514a5bff91570a0c82210f26e2
hasibferdous <hasibferdous.ict@gmail.com>
2022-10-26 14:10:42 +0600

    course api data added

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ app.use(cors())
 
 const categories = require('./data/categories.json');
 
-app.get('/categories', (req, res) =>{
+const courses = require('./data/courses.json');
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const CategoryCourse = courses.filter( n => n.category_id === id);
+    res.send(CategoryCourse);
+})
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourses = courses.find( n => n._id === id);
+    //console.log(selectedCourses);
+    res.send(selectedCourses);
+})
+
+app.get('/explore-categories', (req, res) =>{
     res.send(categories);
 })
 
@@ -15,6 +34,11 @@ app.get('/categories', (req, res) =>{
 app.get('/', (req, res) => {
   res.send('Course API running')
 })
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
